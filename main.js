c14c27d5bd3f6f26d2fce0623ba58aa89db6b663
filipbli <101514797+filipbli@users.noreply.github.com>
2022-05-17 20:17:06 +0200

    Dodanie buttona powrot i funckje scrollto

---
+++ main.js
@@ -0,0 +1,4 @@
+const scrollToSection = () => window.scrollTo({top: 500,  behavior: 'smooth'})
+const scrollToSection2 = () => window.scrollTo({top: 1300,  behavior: 'smooth'})
+const scrollToSection3 = () => window.scrollTo({top: 2200,  behavior: 'smooth'})
+const toTop = () => window.scrollTo({top: 0,  behavior: 'smooth'})
